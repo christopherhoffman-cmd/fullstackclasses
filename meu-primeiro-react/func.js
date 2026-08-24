158ce16c3adef3548f52d34e3b7bd1851a92930e
CHRISTOPHER HARRIS ALVES HOFFMAN <023.985555@universidadesenaicimatec.edu.br>
2026-08-24 12:28:41 -0300

    Meu primeiro vite

---
+++ meu-primeiro-react/func.js
@@ -0,0 +1,6 @@
+function verNome() {
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    document.getElementById("Resultado").innerHTML = "O nome é: " + nome + "<br> O email é: " + email;
+}
+
