28924d2afc1997224a839940bd2947e0831699b7
CHRISTOPHER HARRIS ALVES HOFFMAN <023.985555@universidadesenaicimatec.edu.br>
2026-08-28 12:27:32 -0300

    imc

---
+++ React/function.js
@@ -0,0 +1,6 @@
+function obterDados() {
+    let nome = imc.getElementById("nome").value;
+    let email = imc.getElementById("email").value;
+    let idade = imc.getElementbyId("idade").value;
+    let sexo = imc.getElementById("sexo").value;
+}
